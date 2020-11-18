e1cc45f78991ab2b2bf9e44eeff8a4b77960b5d6
daniela <danielaocampoo@gmail.com>
2020-11-18 10:53:35 -0500

    correcion default role

--- app/models/User.js
+++ app/models/User.js
@@ -2,36 +2,36 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
 const UserSchema = new mongoose.Schema({
-    name:{
+    name: {
         type: String,
         required: true
     },
-    id_user:{
-        type:String,
+    id_user: {
+        type: String,
         required: true,
         unique: true
     },
-    email:{
-        type:String,
-        unique:true,
+    email: {
+        type: String,
+        unique: true,
         required: true
     },
-    password:{
-        type:String,
-        required:true
+    password: {
+        type: String,
+        required: true
     },
-    sign_up_date:{
+    sign_up_date: {
         type: Date,
         default: Date.now()
     },
-    last_login_date:{
-        type:Date,
+    last_login_date: {
+        type: Date,
         default: Date.now()
     },
-    role:{
-        type:String,
-        default:"regular",
-        enum:[
+    role: {
+        type: String,
+        default: "admin",
+        enum: [
             "admin",
             "Gerente general",
             "Asistente de gerencia",
@@ -41,11 +41,11 @@ const UserSchema = new mongoose.Schema({
             "Supervisor de taller",
             "Tecnico de taller"
         ]
-        }
-    
+    }
+
 });
 // este metodo ejecuta un hook antes de un metodo
-UserSchema.pre('save', function (next) {
+UserSchema.pre('save', function(next) {
     bcrypt.genSalt(10).then(salts => {
         //me encriptara una cadena de caracteres, me devuelve una promesa con el hash , y ese hash lo guardo
         bcrypt.hash(this.password, salts).then(hash => {
