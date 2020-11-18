1ecead9211a0db1f325803e03b8b8185d5aa6d60
daniela <danielaocampoo@gmail.com>
2020-11-18 11:51:28 -0500

    modificacion app-private

--- app/controllers/UserController.js
+++ app/controllers/UserController.js
@@ -2,21 +2,21 @@ const Userc = require('../models/User');
 const CONFIG = require('../config/config');
 const jwt = require('jsonwebtoken');
 
-function index(req,res){
+function index(req, res) {
     // busco todos los users y si no da error me devuelve arreglo users
-    Userc.find({}).then(users =>{
+    Userc.find({}).then(users => {
         // si hay usuarios envio codigo de aceptacion y un cuerpo con los prdctos
-        if(users.length) return res.status(200).send({users});
+        if (users.length) return res.status(200).send({ users });
         //en caso de que no hayan datos se manda un codigo y un mensaje xD
-        return res.status(204).send({message: 'NO CONTENT'});
-    }).catch(error=> res.status(500).send({error}));
+        return res.status(204).send({ message: 'NO CONTENT' });
+    }).catch(error => res.status(500).send({ error }));
 }
 
-function create(req,res){
+function create(req, res) {
     //se inicializa una variable con los datos de mi body
     let usuario = new Userc(req.body);
     //guardo con el metodo save el nuevo usuario
-    usuario.save().then(user => { 
+    usuario.save().then(user => {
         payload = { //se debe meter fecha de entrega
             email: user.email,
             name: user.name,
@@ -25,48 +25,48 @@ function create(req,res){
         }
         const token = jwt.sign(payload, CONFIG.SECRET_TOKEN); // aca se deberia de poner la duración del token y demas
 
-        return res.status(201).send({user, token});
-    }).catch(error => res.status(500).send({message: "El usuario ya existe", error}));
+        return res.status(201).send({ user, token });
+    }).catch(error => res.status(500).send({ message: "El usuario ya existe", error }));
 }
 
 
-function show(req,res){
-    if(req.body.error) return res.status(500).send({error});
-    if(!req.body.users) return res.status(404).send({message: 'Not Found :"V'});
+function show(req, res) {
+    if (req.body.error) return res.status(500).send({ error });
+    if (!req.body.users) return res.status(404).send({ message: 'Not Found :"V' });
     let users = req.body.users;
-    return res.status(200).send({users});
+    return res.status(200).send({ users });
 }
 
-function update(req,res){
-    if(req.body.error) return res.status(500).send({error});
+function update(req, res) {
+    if (req.body.error) return res.status(500).send({ error });
     //Se valida si no hay Users.
-    if(!req.body.users) return res.status(404).send({message: 'NOT FOUND'});
+    if (!req.body.users) return res.status(404).send({ message: 'NOT FOUND' });
     let ussuario = req.body.users[0];
     //creo un nuevo objeto con las cosas que quiero cambiarle
-    ussuario = Object.assign(ussuario,req.body);
-    ussuario.save().then(user => res.status(200).send({message: "UPDATED",user})).catch(error => res.status(500).send({error}));
+    ussuario = Object.assign(ussuario, req.body);
+    ussuario.save().then(user => res.status(200).send({ message: "UPDATED", user })).catch(error => res.status(500).send({ error }));
 }
 
-function remove(req,res){
-    if(req.body.error) return res.status(500).send({error});
-    if(!req.body.users) return res.status(404).send({message: 'NOT FOUND'});
-    req.body.users[0].remove().then(user => res.status(200).send({message: "REMOVED", user})).catch(error => res.status(500).send({error}));
+function remove(req, res) {
+    if (req.body.error) return res.status(500).send({ error });
+    if (!req.body.users) return res.status(404).send({ message: 'NOT FOUND' });
+    req.body.users[0].remove().then(user => res.status(200).send({ message: "REMOVED", user })).catch(error => res.status(500).send({ error }));
 }
 
 
 // como buscar se repite en show, update y remove hago una funcion
 // es como un middleware el cual es el que se ejecuta en medio de otros controladores
-function find(req,res,next){
+function find(req, res, next) {
     //se hara lo siguiente para entrar por ejemplo (categoria: 'Hogar')
     let query = {};
-    query[req.params.key]= req.params.value;
+    query[req.params.key] = req.params.value;
     Userc.find(query).then(users => {
         //si no existen users
-        if(!users.length) return next();
+        if (!users.length) return next();
         // en caso de que si haya , se crea un user en el body (no existia)
         req.body.users = users;
         return next();
-    }).catch(error =>{
+    }).catch(error => {
         req.body.error = error;
         next();
     });
@@ -74,58 +74,57 @@ function find(req,res,next){
 }
 
 
-function showTeam(req,res){
+function showTeam(req, res) {
     res.json([{
-        _id: 1, // datos publicos 
-        name: 'Daniela Ocampo',
-        description: 'Gerente general',
-        date: "2020-10-25T01:43:19.346Z"
-    },
-    {
-        _id: 2,
-        name: 'Arcangel  Marin',
-        description: 'Asistente de gerencia',
-        date: "2020-10-25T01:43:19.346Z"
-    },
-    {
-        _id: 3,
-        name: 'Pedro Rios',
-        description: 'Gerente financiero',
-        date: "2020-10-25T01:43:19.346Z"
-    },
-    {
-        _id: 4,
-        name: 'Jhon Vasquez ',
-        description: 'Asistente financiero',
-        date: "2020-10-25T01:43:19.346Z"
-    },
-    {
-        _id: 4,
-        name: 'Samuel Gil ',
-        description: 'Técnico',
-        date: "2020-10-25T01:43:19.346Z"
-    }
-]);
+            _id: 1, // datos publicos 
+            name: 'Daniela Ocampo',
+            description: 'Gerente general',
+            date: "2020-10-25T01:43:19.346Z"
+        },
+        {
+            _id: 2,
+            name: 'Arcangel  Marin',
+            description: 'Asistente de gerencia',
+            date: "2020-10-25T01:43:19.346Z"
+        },
+        {
+            _id: 3,
+            name: 'Pedro Rios',
+            description: 'Gerente financiero',
+            date: "2020-10-25T01:43:19.346Z"
+        },
+        {
+            _id: 4,
+            name: 'Jhon Vasquez ',
+            description: 'Asistente financiero',
+            date: "2020-10-25T01:43:19.346Z"
+        },
+        {
+            _id: 4,
+            name: 'Samuel Gil ',
+            description: 'Técnico',
+            date: "2020-10-25T01:43:19.346Z"
+        }
+    ]);
 
 }
 
-function privateTasks(req,res){
+function privateTasks(req, res) {
     let query = {};
-    query["role"]= "admin";
+    query["role"] = "admin";
     Userc.find(query).then(users => {
         //si no existen users
-        if(!users.length) return next();
+        if (!users.length) return res.send({ message: "no hay ningun usuario con ese rol" });
         // en caso de que si haya , se crea un user en el body (no existia)
-         res.send({users});
-       
-    }).catch(error =>{
-        req.body.error = error;
-        next();
+        res.send({ users });
+
+    }).catch(error => {
+        res.send({ error });
     });
 
 }
 
-function showProfile(req,res){
+function showProfile(req, res) {
     res.send(req.body.rol);
 }
 
@@ -139,18 +138,17 @@ function verifyToken(req, res, next) {
     if (token === 'null') {
         return res.status(401).send('no posee token para esta Request');
     }
-    jwt.verify(token, CONFIG.SECRET_TOKEN, function(error,decoded){
-        if(error) return res.status(403).send({message: 'Fallo al decodificar token',error});
-            //req.userId = payload._id;
-        if(decoded.role == "admin"){
+    jwt.verify(token, CONFIG.SECRET_TOKEN, function(error, decoded) {
+        if (error) return res.status(403).send({ message: 'Fallo al decodificar token', error });
+        //req.userId = payload._id;
+        if (decoded.role == "admin") {
             req.body.rol = decoded.role;
             next();
-        } 
-        else  return res.status(401).send('No tiene el rol necesario para esta Request');
-        });
+        } else return res.status(401).send('No tiene el rol necesario para esta Request');
+    });
 
 
-    
+
 }
 
 
--- app/routes/user.js
+++ app/routes/user.js
@@ -3,15 +3,14 @@ const UserCtrl = require('../controllers/UserController');
 const routerU = express.Router();
 
 //le vamos a dar al router algunas rutas //ejemplos
-routerU.get('/showWorkers', UserCtrl.index)  // api.com/user/  #Index: listar todos los users
-      .post('/', UserCtrl.create)   // api.com/user/  #Create: crear un nuevo users
-      //lo siguiente sera mandarle como un middleware prepararlo y se manda
-      .get('/:key/:value',UserCtrl.find,UserCtrl.show)    // api.com/user/category/Hogar #show: muestra un user en especifico
-      .put('/:key/:value', UserCtrl.find,UserCtrl.update)    // api.com/user/name/SamsungGalaxy #update : actualizar un user en especifico
-      .delete('/:key/:value',UserCtrl.find, UserCtrl.remove) // api.com/user/name/SamsungGalaxy
-      .get('/team',UserCtrl.showTeam)
-      .get('/private-tasks',UserCtrl.verifyToken,UserCtrl.privateTasks)
-      .get('/profile',UserCtrl.verifyToken,UserCtrl.showProfile)
+routerU.get('/showWorkers', UserCtrl.index) // api.com/user/  #Index: listar todos los users
+    .post('/', UserCtrl.create) // api.com/user/  #Create: crear un nuevo users
+    //lo siguiente sera mandarle como un middleware prepararlo y se manda
+    .get('/:key/:value', UserCtrl.find, UserCtrl.show) // api.com/user/category/Hogar #show: muestra un user en especifico
+    .put('/:key/:value', UserCtrl.find, UserCtrl.update) // api.com/user/name/SamsungGalaxy #update : actualizar un user en especifico
+    .delete('/:key/:value', UserCtrl.find, UserCtrl.remove) // api.com/user/name/SamsungGalaxy
+    .get('/team', UserCtrl.showTeam)
+    .get('/private-tasks' /*,UserCtrl.verifyToken*/ , UserCtrl.privateTasks)
+    .get('/profile', UserCtrl.verifyToken, UserCtrl.showProfile)
 
 module.exports = routerU;
-
